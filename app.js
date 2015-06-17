19aa496852870b83e7b754df1cc5daaa9285578d
richardalexandermichels <richard.alexandermichels@gmail.com>
2015-06-17 12:56:00 -0400

    added index html

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express')
 var logger = require('morgan');
 var app = express()
+var swig = require('swig')
 
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', './views');
+swig.setDefaults({ cache: false });
 // create "middleware"
 app.use( logger('dev') );
 
 console.log('server on')
 
 app.get('/', function (req, res) {
-  res.send('Hello earth!');
+  
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 app.get('/any', function (req, res) {
   res.send('Hello any!');
