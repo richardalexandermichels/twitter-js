61a58c85876b7e3693a0c5d0ef216198258e8db8
richardalexandermichels <richard.alexandermichels@gmail.com>
2015-06-17 12:21:21 -0400

    files tracked

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var logger = require('morgan');
+var app = express()
+
+// create "middleware"
+app.use( logger('dev') );
+
+console.log('server on')
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.get('/any', function (req, res) {
+  res.send('Hello any!');
+});
+
+app.listen(3000)
